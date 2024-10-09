cecb945498031448789a276a19a5512ee890590c
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-09 10:50:44 +0200

    Segundo selector anidado - Países

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interfaces/country.interfaces';
@@ -7,7 +7,7 @@ import { Region } from '../../interfaces/country.interfaces';
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit {
   private fb: FormBuilder = new FormBuilder();
 
   public myForm: FormGroup = this.fb.group({
@@ -18,7 +18,17 @@ export class SelectorPageComponent {
 
   constructor(private countriesService: CountriesService) {}
 
+  ngOnInit(): void {
+    this.onRegionChange();
+  }
+
   get regions(): Region[] {
     return this.countriesService.regions;
+  }
+
+  onRegionChange(): void {
+    this.myForm.get('region')!.valueChanges.subscribe((region) => {
+      console.log({ region });
+    });
   }
 }
