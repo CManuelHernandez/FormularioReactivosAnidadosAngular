6754cfd9f486cac41b5a2127f2926d2b20e07a33
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-09 10:26:58 +0200

    Formulario reactivo - primer selector

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
 })
-export class SelectorPageComponent {}
+export class SelectorPageComponent {
+  private fb: FormBuilder = new FormBuilder();
+
+  public myForm: FormGroup = this.fb.group({
+    region: ['', [Validators.required]],
+    country: ['', [Validators.required]],
+    bordes: ['', [Validators.required]],
+  });
+}
