831c1deefb5ead4c0eccbd12f9f6eb2423568bc9
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-09 10:41:55 +0200

    Selector de regiones

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interfaces';
 
 @Component({
   selector: 'app-selector-page',
@@ -13,4 +15,10 @@ export class SelectorPageComponent {
     country: ['', [Validators.required]],
     bordes: ['', [Validators.required]],
   });
+
+  constructor(private countriesService: CountriesService) {}
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
 }
